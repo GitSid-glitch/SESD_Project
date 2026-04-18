f0a5b060640a337cc445c2174dcd30a87292d054
Siddharth <siddharth.shukla@adypu.edu.in>
2026-04-18 13:30:00 +0530

    Add src/types/node-shims.d.ts

---
+++ src/types/node-shims.d.ts
@@ -0,0 +1,14 @@
+declare var require: any;
+declare var module: any;
+declare var process: any;
+declare var __dirname: string;
+declare var Buffer: any;
+declare var console: any;
+declare var URLSearchParams: any;
+
+declare module "fs";
+declare module "path";
+declare module "http";
+declare module "crypto";
+declare module "node:test";
+declare module "node:assert/strict";
